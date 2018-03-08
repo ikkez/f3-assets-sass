499f01686e36f5aa700cd83aa364bcdaeea4905e
ikkez <ikkez0n3@gmail.com>
2018-03-08 16:48:43 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# SASS addon for F3 Sugar Assets
+This is an extention to add a SASS/SCSS compiler to the existing [Assets Management plugin](https://github.com/ikkez/f3-assets) for the [PHP Fat-Free Framework](https://github.com/bcosca/fatfree).
+
+## Install
+
+When you have F3-Assets already up and running, just run `composer require ikkez/f3-assets-sass`. 
+In case you do not use composer, copy the `assets/` folder into your `AUTOLOAD` path and you should be ready to go.
+
+
+## Usage
+
+To register the sass compiler, just add this line to your view controller, or where ever you have put the initialisation of the main assets plugin. 
+
+```php
+// register sass handler
+\Assets\Sass::instance()->init();
+```
+
+Within your templates you can then easily use `.scss` files directly, as it would be normal css files.
+
+```html
+<link rel="stylesheet" href="scss/main_styles.scss">
+```
+
+That's it. Compilation, minification and concatenation with other files is now handled by the assets plugin.
+The base directory of the sass file can also be used as import path, 
+so using `@import` within your sass file can be used to load other relative sass files. So recompiling a whole bootstrap frontend is not problem:
+
+```html
+<link rel="stylesheet" href="components/MDBootstrap/sass/mdb.scss">
+```
+
+The only drawback with `@include` files is, that changes to those files are currently not detected automatically, 
+so the whole main sass file does not update on the fly. However, you can add the `watch` attribute and define 
+one or multiple paths to scan for file changes - wildcards are possible:
+ 
+```html
+<link rel="stylesheet" href="components/MDBootstrap/sass/mdb.scss" watch="custom.scss">
+
+<link rel="stylesheet" href="components/MDBootstrap/sass/mdb.scss" watch="custom.scss, addons/*.scss">
+```
+
+NB: The `watch`-attribute should only be considered while working on the files, because scanning for file modification times isn't necessary for production-ready styles and would, depending on the amount of files to scan, slow things down. 
+If you need to refresh files on a production environment, it's recommended to clear the temp files with `\Assets::instance()->clear();`, instead of proactive looking for changes.
+
+
+License
+-
+
+GPLv3
---
+++ lib/assets/sass.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ *	SASS plugin for Sugar Asset manager
+ *
+ *	The contents of this file are subject to the terms of the GNU General
+ *	Public License Version 3.0. You may not use this file except in
+ *	compliance with the license. Any of the license terms and conditions
+ *	can be waived if you get permission from the copyright holder.
+ *
+ *	Copyright (c) 2017-2018 ~ ikkez
+ *	Christian Knuth <ikkez0n3@gmail.com>
+ *
+ *	@version: 1.0.1
+ *	@date: 24.08.2017
+ *
+ **/
+namespace Assets;
+
+use \Leafo\ScssPhp\Compiler;
+
+class Sass extends \Assets {
+
+	function init() {
+		// get existing instance
+		$assets = \Assets::instance();
+
+		// register sass handler
+		/** @var \Base $f3 */
+		$f3 = \Base::instance();
+
+		$assets->filter('sass',function($collection) use($f3,$assets) {
+
+			// check final path
+			$public_path = $f3->get('ASSETS.public_path');
+			if (!is_dir($public_path))
+				mkdir($public_path,0777,true);
+			foreach($collection as $i=>&$asset) {
+				if ($asset['origin']=='inline' || $asset['origin'] == 'external')
+					continue;
+				$path = $asset['path'];
+				if (is_file($path)
+					&& preg_match('/.*\.s(a|c)ss(?=[?#].*|$)/i',$path)) {
+					// proceed
+					$path_parts = pathinfo($path);
+					$filename = $path_parts['filename'].'.css';
+					$watch=[];
+					if (isset($asset['watch']))
+						foreach ($f3->split($asset['watch']) as $scanPath)
+							$watch+=glob($path_parts['dirname'].'/'.$scanPath);
+					$watch[]=$path_parts['dirname'].'/'.$path_parts['basename'];
+					if (!is_file($public_path.$filename) || (
+							($cmtime=filemtime($public_path.$filename)) &&
+							(bool) implode(array_map(function($path) use ($cmtime){
+								return filemtime(realpath($path)) > $cmtime;
+							},$watch))
+						)) {
+						$scss = new Compiler();
+						$scss->addImportPath($path_parts['dirname']);
+						$css = $scss->compile($f3->read($path));
+						$css = $assets->fixRelativePaths($css,$path_parts['dirname'].'/',$public_path);
+						$f3->write($public_path.$filename,$css);
+					}
+					$asset['path'] = $public_path.$filename;
+					$asset['type'] = 'css';
+				}
+				unset($asset);
+			}
+			return $collection;
+		});
+
+		$assets->formatter('sass',function($asset) use($f3,$assets) {
+			if ($asset['origin']=='inline')
+				return sprintf('<style type="text/css">%s</style>',$asset['data']);
+			$path = $asset['path'];
+			unset($asset['path'],$asset['origin'],$asset['type'],
+				$asset['exclude'],$asset['slot']);
+			$params=$assets->resolveAttr($asset+array(
+					'rel'=>'stylesheet',
+					'type'=>'text/css',
+					'href'=>$path,
+				));
+			return sprintf('<link%s/>',$params);
+		});
+
+		$f3->set('ASSETS.filter.sass','sass');
+	}
+}
